8c30f9a6a4da5613314d788fb1c10fadebc89112
Andres Fajardo <andres.mauricio94@hotmail.com>
2023-08-19 11:37:19 -0500

    mockoon api config

---
+++ src/app/models/articles.ts
@@ -0,0 +1,24 @@
+export interface ArticlesData {
+  articles: Article[];
+  articlesCount: number;
+}
+
+export interface Article {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  favorited: boolean;
+  favoritesCount: number;
+  author: Author;
+}
+
+export interface Author {
+  username: string;
+  bio?: string;
+  image: string;
+  following: boolean;
+}
