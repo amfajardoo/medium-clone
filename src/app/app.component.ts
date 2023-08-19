83e006a276b7d94fd536be610e6853518ad04be1
Andres Fajardo <andres.mauricio94@hotmail.com>
2023-08-19 11:37:10 -0500

    add ESLint Prettier config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { APP_IMPORTS } from '@common/imports';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: APP_IMPORTS,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'medium-clone';
-}
+export class AppComponent {}
